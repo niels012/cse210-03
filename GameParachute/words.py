be13fc96e57ff2057a0eb6266fc923d7d5043e1f
Nilo Urmeneta <urmenetanilo@gmail.com>
2022-02-09 01:27:59 +0800

    merged player input and word file

--- GameParachute/words.py
+++ GameParachute/words.py
@@ -1,8 +1,57 @@
+import random
+
 class Words:
-    def word_list():
-        pass
-    def random_word():
-        pass
-    def update_word():
-        pass
-    
+    def __init__(self):
+        self.is_playing = True
+        self.word = ""
+        self.num_letter = 0
+        self.guess_letter = ""
+        self.guess_wrong = 0
+        self.score = 0
+    def start(self):
+        self.word_picker()
+        while self.is_playing == True:
+            self.output()
+            self.compare_guess()
+    def word_picker(self):
+        words_list = ["ox","cat","deer","mouse","rabbit","carabao","kangaroo"]
+        # words_list =["door","deer","drag","born","bark","crab","roll","mock","spray"]
+        pos_num = random.randint(0,4)
+        picked_word = words_list[pos_num]
+        self.word = picked_word 
+        num_letter = len(self.word)
+        self.num_letter = num_letter
+    def output(self):
+        if self.score == 0:
+            for i in range(self.num_letter):
+                print("_" ,end=" ")
+            print()
+        else:
+            for j in range(self.score):
+                print(self.word[j], end=" ")
+            for blank in range(self.num_letter - self.score):
+                print("_", end=" ")
+            print()
+
+    def get_input(self):
+        letter = input("Guess the letter: ")
+        self.guess_letter = letter
+    def compare_guess(self):
+            self.get_input()
+            if self.guess_letter == self.word[self.score]:
+                self.score += 1
+                if self.score == self.num_letter:
+                    self.output()
+                    self.is_playing = False
+                    print("You Win !!")
+            else:
+                self.guess_wrong += 1
+                if self.guess_wrong == 4:
+                    print("Game Over !!")
+                    self.is_playing = False
+                else:
+                    print(f"You have {self.guess_wrong} mistake")
+                    pass
+
+words = Words()
+words.start()
