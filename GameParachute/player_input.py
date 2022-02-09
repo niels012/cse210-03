0d9aa74ff40c89caa1cf538878ddc0975daa7475
Agustin Aguilar <aguilarag98@gmail.com>
2022-02-09 10:05:23 -0300

    comments added to player_input

--- GameParachute/player_input.py
+++ GameParachute/player_input.py
@@ -2,7 +2,23 @@ from operator import indexOf
 from words import Words
 
 class Player:
+    """A person who plays the game.
+
+    The responsibility of the Player is to obtain user input and compare it with the game's word.
+
+    Attributes:
+        user_guess= User's guess
+        index= Index of the User's guessed letter in the game's word.
+        guess_wrong= Counter of user's wrong guesses.
+        word (Words): The game's word.
+    """
+
     def __init__(self):
+        """Constructs a new Player.
+        
+        Args:
+            self (Player): an instance of Player.
+        """
         self._user_guess=' '
         self.index=0
         self.guess_wrong=0
@@ -10,11 +26,20 @@ class Player:
         
         pass
     def get_input(self):
-        # letter = input("Guess the letter: ")
+        """Gets user's input (a letter).
+
+        Args:
+            self (Player): an instance of Player.
+        """
         self._user_guess=input("Guess the letter: ")
         pass
     def compare_guess(self):
-        # self.func.word_picker()
+        """Check if user's input (self._user_guess) is inside the word provided by 
+            the Words class (self.word)
+
+        Args:
+            self (Player): an instance of Player.
+        """
         self.get_input()
         if self._user_guess in self.word:
             self.guess_right()
@@ -22,14 +47,26 @@ class Player:
             self.wrong()
         pass
     def guess_right(self):
-        # self.func.word_picker()
+        """Updates self.index with the word's index where the user's chosen letter
+            is in.
+
+        Args:
+            self (Player): an instance of Player.
+        """
         word=self.word
         guess=self._user_guess
         self.index= indexOf(word,guess)
         self.word[self.index]=self._user_guess
         pass
     def wrong(self):
+        """Adds 1 to the self.guess_wrog counter
+
+        Args:
+            self (Player): an instance of Player.
+        """
         self.guess_wrong+=1
         pass
 
-Player().compare_guess()
+
+#This last line was just to test the class. Erase when the program is finished
+# Player().compare_guess()
