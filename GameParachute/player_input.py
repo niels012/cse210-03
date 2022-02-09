c31c402e012dc2478e8230739d7363e02c09cafd
Agustin Aguilar <aguilarag98@gmail.com>
2022-02-09 09:51:08 -0300

    Player class created. Words class modified

--- GameParachute/player_input.py
+++ GameParachute/player_input.py
@@ -1,9 +1,35 @@
+from operator import indexOf
+from words import Words
+
 class Player:
-    def get_input():
+    def __init__(self):
+        self._user_guess=' '
+        self.index=0
+        self.guess_wrong=0
+        self.word=Words().word_picker()
+        
         pass
-    def compare_guess():
+    def get_input(self):
+        # letter = input("Guess the letter: ")
+        self._user_guess=input("Guess the letter: ")
         pass
-    def guess_right():
+    def compare_guess(self):
+        # self.func.word_picker()
+        self.get_input()
+        if self._user_guess in self.word:
+            self.guess_right()
+        else:
+            self.wrong()
         pass
-    def guess_wrong():
+    def guess_right(self):
+        # self.func.word_picker()
+        word=self.word
+        guess=self._user_guess
+        self.index= indexOf(word,guess)
+        self.word[self.index]=self._user_guess
         pass
+    def wrong(self):
+        self.guess_wrong+=1
+        pass
+
+Player().compare_guess()
