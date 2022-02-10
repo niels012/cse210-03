10f4728bfe9c3a95d93d96b06087035af6217b95
Cocotero98 <aguilarag98@gmail.com>
2022-02-10 15:35:17 -0300

    Game finishied!... almost

--- GameParachute/Game/construct_para.py
+++ GameParachute/Game/construct_para.py
@@ -2,26 +2,80 @@ class Parachute:
 
     def __init__(self):
         """Setting the initial value"""
-        self._parachute = ["  ___ \n /___\ \n \   / \n  \ / \n   0 \n  /|\ \n  / \ \n \n^^^^^^^"]
+        self._parachute = "  ___ \n /___\ \n \   / \n  \ / \n   0 \n  /|\ \n  / \ \n \n^^^^^^^"
+        self._word_space = ""
 
-    def update_parachute (self):
-        """Updating the parachute output according to the answers"""
-        if self.guess_wrong == 0:
-            self._parachute = ["  ___ \n /___\ \n \   / \n  \ / \n   0 \n  /|\ \n  / \ \n \n^^^^^^^"]
-        elif self.guess_wrong == 1:
-            self._parachute = [" /___\ \n \   / \n  \ / \n   0 \n  /|\ \n  / \ \n \n^^^^^^^"]
-        elif self.guess_wrong == 2:
-            self._parachute = [" \   / \n  \ / \n   0 \n  /|\ \n  / \ \n \n^^^^^^^"]
-        elif self.guess_wrong == 3:
-            self._parachute = ["  \ / \n   0 \n  /|\ \n  / \ \n \n^^^^^^^"]
+    def update_parachute (self,guess_wrong,word,letter,index):
+        """
+        Updating the parachute output according to the answers
+        
+        Args:
+             self (Parachute): an instance of Parachute.
+             guess_wrong (integer): Counter of the user's wrong attempts.
+             word (str): a word provided by the game.
+             letter (str): a letter provided by the user.
+             index (list): a list of numbers (indices).
+        """
+        self.word_line(word,letter,index)
+        if guess_wrong == 0:
+            self._parachute = f"{self._word_space}\n\n  ___ \n /___\ \n \   / \n  \ / \n   0 \n  /|\ \n  / \ \n \n^^^^^^^"
+        elif guess_wrong == 1:
+            self._parachute = f"{self._word_space}\n\n /___\ \n \   / \n  \ / \n   0 \n  /|\ \n  / \ \n \n^^^^^^^"
+        elif guess_wrong == 2:
+            self._parachute = f"{self._word_space}\n\n \   / \n  \ / \n   0 \n  /|\ \n  / \ \n \n^^^^^^^"
+        elif guess_wrong == 3:
+            self._parachute = f"{self._word_space}\n\n  \ / \n   0 \n  /|\ \n  / \ \n \n^^^^^^^"
         else:
-            self._parachute = ["   x \n  /|\ \n  / \ \n \n^^^^^^^"]
+            self._parachute = "   x \n  /|\ \n  / \ \n \n^^^^^^^"
         
     def display_parachute (self):
-        """ Displaying the updated parachute"""
+        """ 
+        Displaying the updated parachute
+        
+        Args:
+             self (Parachute): an instance of Parachute.
+        """
         print(self._parachute)
+
+    def word_line(self,word,letter,index):
+        '''
+        Creates the slots for the game's word. Also replaces the slot with the user's
+        chosen letter if correct
+
+        Args:
+             self (Parachute): an instance of Parachute.
+             word (str): a word provided by the game.
+             letter (str): a letter provided by the user.
+             index (list): a list of numbers (indices).
+        '''
+        # If the lenght of our word space (ex: '_ _ _' (each underscore and space between them 
+        # is part of the length. In this example would be 5)) +1(6), /2(3) is not equal to the length of the 
+        # game's word (According to the example, also 3) AND the length of word_space /2 is not equal 
+        # to the game's word: We create a line of underscores and spaces.
+
+        if (len(self._word_space)+1)/2!=len(word) and len(self._word_space)/2!=len(word):
+            for i in range(len(word)):
+                self._word_space+='_ '
+        # If the letter is in the word we create an empty list
+        if letter in word:
+            new_list=[]
+            # We append each element of the word to the list except for white spaces(' ')
+            for j in self._word_space:
+                if j !=' ':
+                    new_list.append(j)
+            # For each number in the index's list, we replace the element with the right index with
+            # the chosen letter.
+            for k in index:
+                new_list[k]=letter
+            # We join the list into a string with a white space between them
+            new_word=' '.join(new_list)
+            self._word_space=new_word
+
         
 
+
+
+# Parachute().display_parachute()
 ### Note! ###
 
 # The "self.guess_wrong" above doesn't need to be that name. Whoever's responsible for counting answers can choose the name they want.
--- GameParachute/Game/player_input.py
+++ GameParachute/Game/player_input.py
@@ -1,5 +1,4 @@
 from operator import indexOf
-from words import Words
 
 class Player:
     """A person who plays the game.
@@ -22,46 +21,49 @@ class Player:
         self._user_guess=' '
         self.indices=[]
         self.guess_wrong=0
-        self.word=Words().word_picker()
-        
         pass
+
     def get_input(self):
         """Gets user's input (a letter).
 
         Args:
             self (Player): an instance of Player.
         """
-        self._user_guess=input("Guess the letter: ")
+        self._user_guess=input("Guess a letter [a-z]: ")
         pass
-    def compare_guess(self):
+
+    def compare_guess(self,game_word):
         """Check if user's input (self._user_guess) is inside the word provided by 
             the Words class (self.word)
 
         Args:
             self (Player): an instance of Player.
+            game_word: a word provided by the game.
         """
         self.get_input()
-        if self._user_guess in self.word:
-            self.guess_right()
+        if self._user_guess in game_word:
+            self.guess_right(game_word)
         else:
             self.wrong()
         pass
-    def guess_right(self):
+
+    def guess_right(self,game_word):
         """Updates self.index with the word's indices where the user's chosen letter
             is in.
 
         Args:
             self (Player): an instance of Player.
+            game_word: a word provided by the game.
         """
-        word=self.word
+        word=game_word
         guess=self._user_guess
         indices=[]
         for i in range(len(word)):
             if word[i] == guess:
                 indices.append(i)
         self.indices= indices
-        print(self.indices)
         pass
+
     def wrong(self):
         """Adds 1 to the self.guess_wrog counter
 
@@ -71,6 +73,3 @@ class Player:
         self.guess_wrong+=1
         pass
 
-
-#This last line was just to test the class. Erase when the program is finished
-Player().compare_guess()
